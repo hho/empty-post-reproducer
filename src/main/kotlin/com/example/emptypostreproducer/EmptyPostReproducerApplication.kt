9af3c2772208aba0a66183ba1bc52c46df5ea744
Henning Hoefer <hho@users.noreply.github.com>
2017-12-08 16:41:49 +0100

    Simple test

--- src/main/kotlin/com/example/emptypostreproducer/EmptyPostReproducerApplication.kt
+++ src/main/kotlin/com/example/emptypostreproducer/EmptyPostReproducerApplication.kt
@@ -1,11 +1,22 @@
 package com.example.emptypostreproducer
 
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.*
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class EmptyPostReproducerApplication
+class EmptyPostReproducerApplication {
+
+	@Bean
+	fun backend() = WireMockServer(9090).apply {
+		stubFor(any(urlEqualTo("/testit")).willReturn(noContent()))
+		start()
+	}
+
+}
 
 fun main(args: Array<String>) {
-    runApplication<EmptyPostReproducerApplication>(*args)
+	runApplication<EmptyPostReproducerApplication>(*args)
 }
--- src/test/kotlin/com/example/emptypostreproducer/EmptyPostReproducerApplicationTests.kt
+++ src/test/kotlin/com/example/emptypostreproducer/EmptyPostReproducerApplicationTests.kt
@@ -1,16 +1,47 @@
 package com.example.emptypostreproducer
 
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.reactive.server.WebTestClient
+
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@SpringBootTest(classes = arrayOf(EmptyPostReproducerApplication::class), webEnvironment = RANDOM_PORT)
 class EmptyPostReproducerApplicationTests {
 
-	@Test
-	fun contextLoads() {
+	@LocalServerPort
+	protected var port = 0
+
+	lateinit var webClient: WebTestClient
+	lateinit var baseUri: String
+
+	@Before
+	fun setup() {
+		baseUri = "http://localhost:" + port
+		this.webClient = WebTestClient.bindToServer().baseUrl(baseUri).build()
 	}
 
+	@Test
+	fun `Post with body`() {
+		webClient.post()
+				.uri("/testit")
+				.header("X-TEST", "true")
+				.syncBody("foo")
+				.exchange()
+				.expectStatus().isNoContent
+	}
+
+	@Test
+	fun `Post without body`() {
+		webClient.post()
+				.uri("/testit")
+				.header("X-TEST", "true")
+				.exchange()
+				.expectStatus().isNoContent
+	}
 }
