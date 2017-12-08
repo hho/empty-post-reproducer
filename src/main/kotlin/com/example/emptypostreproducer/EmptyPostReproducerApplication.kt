5d3bcebbdc400771df42d9c5642da41079245672
Henning Hoefer <hho@users.noreply.github.com>
2017-12-08 16:38:01 +0100

    Initial commit (start.spring.io)

---
+++ src/main/kotlin/com/example/emptypostreproducer/EmptyPostReproducerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.emptypostreproducer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmptyPostReproducerApplication
+
+fun main(args: Array<String>) {
+    runApplication<EmptyPostReproducerApplication>(*args)
+}
